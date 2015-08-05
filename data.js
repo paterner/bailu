10c50a8356c76b291e07df3eb8ce716b961bb065
jiangnan <229142144@qq.com>
2015-08-05 21:13:29 +0800

    test

---
+++ data.js
@@ -0,0 +1,24 @@
+
+//导航数据
+var leftNavCode = [
+    {
+        title : "我的工作",
+        id : "mytask1",
+        pos : 0,
+        child : [{title:"任务1",tag:"task1"},{title:"任务2",tag:"task2"},{title:"任务3",tag:"task3"}]
+    },
+    {
+        title : "项目管理",
+        id : "mytask2",
+        pos : -60,
+        child : [{title:"任务1",tag:"task1"},{title:"任务2",tag:"task2"}, {title:"任务3",tag:"task3"} ]
+    },
+    {
+        title : "子系统配置",
+        id : "mytask3",
+        pos : -120,
+        child : [{title:"任务1",tag:"task1"},{title:"任务2",tag:"task2"}, {title:"任务3",tag:"task3"}]
+    }
+
+]
+
---
+++ data_a.js
@@ -0,0 +1,152 @@
+// 任务树模拟数据
+
+
+var tmp_data = [
+    {
+        "date": "2015-2-4",
+        "tag": "start"
+    },
+    {
+        "date": "2015-02-15",
+        "class_1": {
+            "class_2": [
+                {
+                    "text": "设计输入条件",
+                    "tag": 2,
+                    "eleId": "1",
+                    "class_3": [
+                        {
+                            "text": "一二三",
+                            "tag": 3,
+                            "class_4": [
+                                {
+                                    "text": "一二三",
+                                    "tag": 4, "eleId": "5"
+                                }
+                            ],
+                            "eleId": "5"
+                        }
+                    ]
+                },
+                {
+                    "text": "设计输入条件",
+                    "tag": 2,
+                    "eleId": "1",
+                    "class_3": [
+                        {
+                            "text": "一二三",
+                            "tag": 3,
+                            "class_4": [
+                                {
+                                    "text": "一二三",
+                                    "tag": 4, "eleId": "5"
+                                }
+                            ],
+                            "eleId": "5"
+                        }
+                    ]
+                },
+                {
+                    "text": "设计输入条件",
+                    "tag": 2,
+                    "eleId": "1",
+                    "class_3": [
+                        {
+                            "text": "一二三",
+                            "tag": 3,
+                            "class_4": [
+                                {
+                                    "text": "一二三",
+                                    "tag": 4, "eleId": "5"
+                                }
+                            ],
+                            "eleId": "5"
+                        }
+                    ]
+                }
+            ],
+            "text": "\u4e13\u4e1a\u534f\u8c03\u4f1a_\u65bd\u5de5\u56fe\u9636\u6bb5",
+            "tag": 1,
+            "eleId": "515786ca269849e1974221d2c97373ba"
+        }
+    },
+    {
+        "date": "2015-02-15",
+        "class_1": {
+            "class_2": [
+                {
+                    "text": "设计输入条件",
+                    "tag": 2,
+                    "eleId": "1",
+                    "class_3": [
+                        {
+                            "text": "一二三",
+                            "tag": 3,
+                            "class_4": [
+                                {
+                                    "text": "一二三",
+                                    "tag": 4, "eleId": "5"
+                                }
+                            ],
+                            "eleId": "5"
+                        }
+                    ]
+                },
+                {
+                    "text": "设计输入条件",
+                    "tag": 2,
+                    "eleId": "1",
+                    "class_3": [
+                        {
+                            "text": "一二三",
+                            "tag": 3,
+                            "class_4": [
+                                {
+                                    "text": "一二三",
+                                    "tag": 4, "eleId": "5"
+                                }
+                            ],
+                            "eleId": "5"
+                        }
+                    ]
+                }
+
+            ],
+            "text": "\u4e13\u4e1a\u534f\u8c03\u4f1a_\u65bd\u5de5\u56fe\u9636\u6bb5",
+            "tag": 1,
+            "eleId": "515786ca269849e1974221d2c97373ba"
+        }
+    },
+    {
+        "date": "2015-02-15",
+        "class_1": {
+            "class_2": [
+                {
+                    "text": "设计输入条件",
+                    "tag": 2,
+                    "eleId": "1",
+                    "class_3": [
+                        {
+                            "text": "一二三",
+                            "tag": 3,
+                            "class_4": [
+                                {
+                                    "text": "一二三",
+                                    "tag": 4, "eleId": "5"
+                                }
+                            ],
+                            "eleId": "5"
+                        }
+                    ]
+                }
+            ],
+            "text": "\u4e13\u4e1a\u534f\u8c03\u4f1a_\u65bd\u5de5\u56fe\u9636\u6bb5",
+            "tag": 1,
+            "eleId": "515786ca269849e1974221d2c97373ba"
+        }
+    },
+    {
+        "date": "2015-2-26",
+        "tag": "end"
+    }
+]
